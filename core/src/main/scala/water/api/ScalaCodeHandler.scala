95e4f13227e65ee3b5a9be4ec90673ebd80811d1
kuba <jakub.hava@gmail.com>
2015-06-26 22:58:17 +0200

    Create timeout for interpreters and sessions

--- core/src/main/scala/water/api/ScalaCodeHandler.scala
+++ core/src/main/scala/water/api/ScalaCodeHandler.scala
@@ -8,6 +8,7 @@ import org.apache.spark.repl.{SparkILoop, SparkIMain}
 import water.Iced
 import water.fvec.NFSFileVec
 
+import scala.compat.Platform
 import scala.tools.nsc.Settings
 import scala.tools.nsc.interpreter.{NamedParam, ILoop, IMain}
 
@@ -18,14 +19,32 @@ import scala.tools.nsc.interpreter.{NamedParam, ILoop, IMain}
 class ScalaCodeHandler(val sc: SparkContext)  extends Handler {
 
   val sparkContext: SparkContext = sc
-  var mapIntr : Map[String, IMain] = Map()
+  var mapIntr : scala.collection.mutable.Map[String, IMain] = scala.collection.mutable.Map()
+  var mapTimes : Map[String,Long] = Map()
+  val timeout = 30000 // 5 minutes in miliseconds
+  val checkThread = new Thread(new Runnable {
+    def run() {
+      while(true){
+        mapTimes.foreach{case (id: String, lastChecked: Long)=>{
+          if(Platform.currentTime-lastChecked>=timeout){
+            mapIntr(id).close()
+            mapIntr -= id
+            mapTimes -= id
+          }
+        }}
+        Thread.sleep(timeout)
+      }
+    }
+  })
+  checkThread.start()
 
   def interpret(version:Int, s: ScalaCodeV3): ScalaCodeResultV3 = {
     val reply = new ScalaCodeResultV3
     if (s.sessionId == null || !mapIntr.isDefinedAt(s.sessionId)) {
-      // session ID not set so far
+      // session ID not set
       reply.response = "Create session ID using the address /3/sessionId"
     } else {
+      mapTimes += s.sessionId -> Platform.currentTime // update the time
       reply.result = mapIntr(s.sessionId).interpret(s.code).toString
       reply.sessionId = s.sessionId
     }
@@ -36,6 +55,7 @@ class ScalaCodeHandler(val sc: SparkContext)  extends Handler {
     val intr = ScalaCodeHandler.initializeInterpreter(sparkContext);
     val id = UUID.randomUUID().toString // simple solution for now ..
     mapIntr += id -> intr
+    mapTimes += id -> Platform.currentTime
     val reply = new ScalaCodeResultV3
     reply.sessionId = id
     reply
